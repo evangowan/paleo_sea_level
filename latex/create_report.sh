bb1c1e8abdb2ecf55069dbeedb2c2343385e3dd5
evangowan <evangowan@gmail.com>
2020-06-26 05:48:26 +0200

    Switched from pdflatex to Xelatex, which allows for easier incorporation of non-English letters and characters

--- latex/create_report.sh
+++ latex/create_report.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 file_name="report"
-#file_name="diff2"
 
 bash figure_tex.sh
 
 rm *.aux *.bbl *.blg *.dvi *.log
 
-pdflatex ${file_name}
+# xelatex defaults to PDF version 1.5, so you have to manually set it to 1.7 to be compatible with psconvert PDF files
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
 bibtex ${file_name}
-pdflatex ${file_name}
-pdflatex ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
 
 
 texcount -inc  ${file_name}.tex

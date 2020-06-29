2caf7d76fce3f9d83b9f3c90f560d54f4494fd7e
evangowan <evangowan@gmail.com>
2020-06-29 05:52:59 +0200

    added Pechora Sea

---
+++ regions/Eurasian_Arctic/Pechora_Sea/plot_parameters.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# center point for Lambert azimuthal projection for Barents-Kara Sea
+center_longitude=45
+center_latitude=69
+
+# corners of the main plot with the locations of the data
+bottom_long=46.4
+bottom_lat=65
+top_long=69
+top_lat=70.7
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JA${center_longitude}/${center_latitude}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=65
+small_west_longitude=46
+small_east_latitude=73
+small_east_longitude=97
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JA${center_longitude}/${center_latitude}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=67.5
+scale_bar_long=49
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=68.4
+# width is in km
+scale_bar_width=100
+
+
+# shift where the insert map should go. 
+insert_position="br" # can be tr or br

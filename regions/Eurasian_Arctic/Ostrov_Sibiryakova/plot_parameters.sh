29b2b2c0a4ca33d3d23f1ee75f0a400f0a113a52
evangowan <evangowan@gmail.com>
2020-06-30 08:24:02 +0200

    added several locations in the Kara Sea and Franz Josef Land area

---
+++ regions/Eurasian_Arctic/Ostrov_Sibiryakova/plot_parameters.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# center point for Lambert azimuthal projection for Barents-Kara Sea
+center_longitude=45
+center_latitude=69
+
+# corners of the main plot with the locations of the data
+bottom_long=75
+bottom_lat=72.5
+top_long=83
+top_lat=73
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JA${center_longitude}/${center_latitude}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=65
+small_west_longitude=46
+small_east_latitude=73
+small_east_longitude=97
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JA${center_longitude}/${center_latitude}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=72.6
+scale_bar_long=77
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=72.8
+# width is in km
+scale_bar_width=50
+
+
+# shift where the insert map should go. 
+insert_position="tr" # can be tr or br

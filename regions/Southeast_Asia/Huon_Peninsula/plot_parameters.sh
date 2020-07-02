8d7bcd9e674f99205eeed981458155a9735e2360
evangowan <evangowan@gmail.com>
2020-07-02 10:23:05 +0200

    added Huon Peninsula

---
+++ regions/Southeast_Asia/Huon_Peninsula/plot_parameters.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# Lambert Conformal Conic
+center_longitude=147
+center_latitude=-7
+south_parallel=-10
+north_parallel=-3
+
+# corners of the main plot with the locations of the data
+bottom_long=146
+bottom_lat=-7.8
+top_long=149
+top_lat=-4.8
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=-12
+small_west_longitude=139
+small_east_latitude=0
+small_east_longitude=155
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=-7.1
+scale_bar_long=147
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=-6.1
+# width is in km
+scale_bar_width=100
+
+
+# shift where the insert map should go. 
+insert_position="tr" # can be tr or br

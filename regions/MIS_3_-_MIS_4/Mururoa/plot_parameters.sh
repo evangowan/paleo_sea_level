746b71e6b2a06209ac9a81fc3f029ab7435fd4f2
evangowan <evangowan@gmail.com>
2020-07-28 09:50:53 +0200

    added Mururoa MIS 3-4

---
+++ regions/MIS_3_-_MIS_4/Mururoa/plot_parameters.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# Lambert Conformal Conic
+center_longitude=-141
+center_latitude=-21
+south_parallel=-25
+north_parallel=-14
+
+# corners of the main plot with the locations of the data
+bottom_long=-141
+bottom_lat=-24.3
+top_long=-136
+top_lat=-19.5
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=-27
+small_west_longitude=-157
+small_east_latitude=-10
+small_east_longitude=-125
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=-23.5
+scale_bar_long=-140
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=-22
+# width is in km
+scale_bar_width=100
+
+
+# shift where the insert map should go. 
+insert_position="tr" # can be tr or br

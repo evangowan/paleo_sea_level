a6be4ba073cf60d147d47513c9bd1884caee3248
evangowan <evangowan@gmail.com>
2021-10-15 12:23:42 +0900

    added Tsushima-Korea Strait

---
+++ regions/MIS_3_-_MIS_4/Tsushima-Korea_Strait/plot_parameters.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# Lambert Conformal Conic
+center_longitude=135
+center_latitude=39.5
+south_parallel=35
+north_parallel=44
+
+# corners of the main plot with the locations of the data
+bottom_long=126.5
+bottom_lat=32.5
+top_long=132.5
+top_lat=36.8
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=30
+small_west_longitude=122
+small_east_latitude=46
+small_east_longitude=152
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=36.3
+scale_bar_long=131.2
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=34.8
+# width is in km
+scale_bar_width=100
+
+
+# shift where the insert map should go. 
+insert_position="br" # can be tr or br

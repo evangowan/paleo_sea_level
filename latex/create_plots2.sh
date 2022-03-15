c7aa3b2adfa9c0f35303ef364df28092c4d8efa0
Evan Gowan <evangowan@gmail.com>
2022-03-15 17:35:29 +0900

    added files for report 2, which were for some reason not already in the repository

---
+++ latex/create_plots2.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+# This uses GMT, so make sure it is installed first!
+
+if [ ! -d plots ]
+then
+  mkdir plots
+fi
+
+if [ ! -d temp ]
+then
+  mkdir temp
+fi
+
+if [ ! -d statistics ]
+then
+  mkdir statistics
+fi
+
+
+rm temp/map_plot_dimensions.txt
+
+reference_ice_model="72_73_74_75"
+reference_earth_model="ehgr"
+
+cat << END_CAT > temp/compare_models.txt
+72_73_74_75 ehgr
+72_73_74_75_h ehgr
+82_83_84_85 ehgr
+72_73_74_75 eb0ggr
+72_73_74_75 efhC
+72_73_74_75 efhl
+END_CAT
+
+rm temp/map_plot_dimensions.txt
+
+for region in $(cat ../regions/region_list.txt)
+do
+
+	number_locations=$(wc -l < ../regions/${region}/location_list.txt)
+	
+
+	for counter in $(seq 1 ${number_locations} )
+	do
+		location=$(awk -v line=${counter} --field-separator '\t' '{if (NR==line) {print $1}}' ../regions/${region}/location_list.txt)
+		subregion=$(awk -v line=${counter} --field-separator '\t' '{if (NR==line) {print $2}}' ../regions/${region}/location_list.txt)
+		source plot_script.sh ${region} ${location} ${reference_ice_model} ${reference_earth_model} ${subregion}
+
+	done
+
+
+
+done
---
+++ latex/create_report2.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+file_name="report2"
+
+bash figure_tex.sh
+
+rm *.aux *.bbl *.blg *.dvi *.log
+
+# xelatex defaults to PDF version 1.5, so you have to manually set it to 1.7 to be compatible with psconvert PDF files
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+bibtex ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+
+
+texcount -inc  ${file_name}.tex
+
+# clean temporary files
+
+rm *.aux *.bbl *.blg *.dvi *.log

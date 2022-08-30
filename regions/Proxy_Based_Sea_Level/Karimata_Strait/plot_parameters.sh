7f1de014b8059a90067f2cb7d6145ba363a07a28
Evan Gowan <evangowan@gmail.com>
2022-08-30 15:07:19 +0900

    added Karimata Strait MIS 3 proxy record

---
+++ regions/Proxy_Based_Sea_Level/Karimata_Strait/plot_parameters.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+
+###############################################################
+# parameters for the regional map plots
+###############################################################
+
+# Lambert Conformal Conic
+center_longitude=106
+center_latitude=6
+south_parallel=-1
+north_parallel=13
+
+# corners of the main plot with the locations of the data
+bottom_long=107.5
+bottom_lat=-3.5
+top_long=110.5
+top_lat=-0.5
+
+R_main="-R${bottom_long}/${bottom_lat}/${top_long}/${top_lat}r"
+J_main="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${width}c"
+
+
+# corners for the inset map
+small_west_latitude=-6
+small_west_longitude=93
+small_east_latitude=17
+small_east_longitude=118
+
+R_insert="-R${small_west_longitude}/${small_west_latitude}/${small_east_longitude}/${small_east_latitude}r"
+J_insert="-JL${center_longitude}/${center_latitude}/${north_parallel}/${south_parallel}/${insert_width}c"
+
+# location of where the scale bar is plotted. Takes some trial and error to get it in the right spot.
+scale_bar_lat=-3.2
+scale_bar_long=109.5
+# this is the latitude where it measures the width of the scale bar. Remember, the width will change depending on latitude!
+scale_bar_reference_lat=-2
+# width is in km
+scale_bar_width=100
+
+
+# shift where the insert map should go. 
+insert_position="tr" # can be tr or br

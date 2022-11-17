5d4252c35fe025ec00e4933542591766e3c67cbd
Evan Gowan <evangowan@gmail.com>
2022-11-17 12:35:15 +0900

    python script to check if there is a different region name for the map rather than indicated

---
+++ latex/python/check_region.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+import numpy as np
+import pandas as pd
+
+location = sys.argv[1]
+location_list_file = sys.argv[2]
+
+col_names = ["location", "subregion", "map_location"]
+
+location_list = pd.read_csv (location_list_file, sep='\t', header=None, names=col_names)
+
+map_location = ""
+for index, row in location_list.iterrows():
+	if location == row['location']:
+		if not pd.isna(row['map_location']):
+			map_location = row['map_location']
+		else:
+			map_location = location
+
+
+print(map_location)

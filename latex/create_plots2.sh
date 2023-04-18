bea558c7fab52e807a6ae45bd53ee4334782f442
Evan Gowan <evangowan@gmail.com>
2023-04-18 16:01:53 +0900

    added reformed report 2

--- latex/create_plots2.sh
+++ latex/create_plots2.sh
@@ -2,40 +2,25 @@
 
 # This uses GMT, so make sure it is installed first!
 
-if [ ! -d plots ]
-then
-  mkdir plots
-fi
-
-if [ ! -d temp ]
-then
-  mkdir temp
-fi
-
-if [ ! -d statistics ]
-then
-  mkdir statistics
-fi
-
-
-rm temp/map_plot_dimensions.txt
-
 # This is where you modify the script to change the calculated sea level, using six different ice sheet/Earth model 
 # combinations. These are placed in the "calculated_sea_level" folder. Look at the readme in that folder.
 
-reference_ice_model="72_73_74_75"
+reference_ice_model="PM_1_A_h"
 reference_earth_model="ehgr"
 
-cat << END_CAT > temp/compare_models.txt
-72_73_74_75 ehgr
-72_73_74_75_h ehgr
-82_83_84_85 ehgr
-72_73_74_75 eb0ggr
-72_73_74_75 efhC
-72_73_74_75 efhl
-END_CAT
 
-rm temp/map_plot_dimensions.txt
+mkdir -p temp
+
+six_models="temp/compare_models.txt"
+
+cat << END_CAT > temp/compare_models.txt
+PM_1_h ehgr
+PM_1_A_h_Ant_A ehgr
+PM_1_A_h eb0ggr
+PM_1_A_h efhC
+PM_1_A_h eegr
+ICE-6G vm5a
+END_CAT
 
 
 for mis in 'MIS_1-2' 'MIS_3-4' #'MIS_5_a_d' 'MIS_5e' # for now, no MIS 5
@@ -50,7 +35,20 @@ do
 		do
 			location=$(awk -v line=${counter} --field-separator '\t' '{if (NR==line) {print $1}}' ../sea_level_data/${region}/location_list.txt)
 
-			source plot_script2.sh  ${region}  ${location} ${mis} ${reference_ice_model} ${reference_earth_model}
+
+			cat << END_CAT > temp/plot_parameters.sh 
+region=${region}
+location=${location}
+mis_stage=${mis}
+reference_ice_model=${reference_ice_model}
+reference_earth_model=${reference_earth_model}
+six_models=${six_models}
+END_CAT
+
+
+
+bash plot_script2.sh  temp/plot_parameters.sh
+
 
 		done
 
--- latex/create_plots_single2.sh
+++ latex/create_plots_single2.sh
@@ -2,93 +2,47 @@
 
 # This uses GMT, so make sure it is installed first!
 
-region_input=$1
+region=$1
 
 location=$2
 
-if [ ! -d plots ]
-then
-  mkdir plots
-fi
+mis=$3
 
-if [ ! -d temp ]
-then
-  mkdir temp
-fi
-
-if [ ! -d statistics ]
-then
-  mkdir statistics
-fi
-
-if [ -f "temp/map_plot_dimensions.txt" ]
-then
-	rm temp/map_plot_dimensions.txt
-fi
-
-reference_ice_model="72_73_74_75"
+reference_ice_model="PM_1_A_h"
 reference_earth_model="ehgr"
 
-cat << END_CAT > temp/compare_models.txt
-72_73_74_75 ehgr
-72_73_74_75_h ehgr
-82_83_84_85 ehgr
-72_73_74_75 eb0ggr
-72_73_74_75 efhC
-72_73_74_75 efhl
+mkdir -p temp
+
+six_models="temp/compare_models.txt"
+
+cat << END_CAT > ${six_models}
+PM_1_h ehgr
+PM_1_A_h_Ant_A ehgr
+PM_1_A_h eb0ggr
+PM_1_A_h efhC
+PM_1_A_h eegr
+ICE-6G vm5a
 END_CAT
 
-found_region=false
-found_location=false
 
-for region in $(cat ../sea_level_data/region_list.txt)
-do
-
-	if [ "${region}" = "${region_input}" ]
-	then
-
-		found_region=true
-
-		number_locations=$(wc -l < ../sea_level_data/${region}/location_list.txt)
-		
-
-		for counter in $(seq 1 ${number_locations} )
-		do
-			location_temp=$(awk -v line=${counter} --field-separator '\t' '{if (NR==line) {print $1}}' ../sea_level_data/${region}/location_list.txt)
-
-			if [ "${location}" = "${location_temp}" ]
-			then
-
-				found_location=true
-				subregion=$(awk -v line=${counter} --field-separator '\t' '{if (NR==line) {print $2}}' ../sea_level_data/${region}/location_list.txt)
-				source plot_script.sh ${region} ${location} ${reference_ice_model} ${reference_earth_model} ${subregion}
-
-			fi
-
-		done
+cat << END_CAT > temp/plot_parameters.sh 
+region=${region}
+location=${location}
+mis_stage=${mis}
+reference_ice_model=${reference_ice_model}
+reference_earth_model=${reference_earth_model}
+six_models=${six_models}
+END_CAT
 
 
 
-	fi
+bash plot_script2.sh  temp/plot_parameters.sh
 
 
-done
 
 
-if [ "${found_region}" == "false"  ]
-then
 
-		echo "invalid region: ${region_input}"
 
-else
-	if [ "${found_location}" == "false"  ]
-	then
-
-			echo "invalid location: ${location}"
-
-	fi
-
-fi
 
 
 

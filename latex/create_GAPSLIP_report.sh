c922702410f638644867801a6f76abbeb521f209
Evan Gowan <evangowan@gmail.com>
2023-04-20 19:26:01 +0900

    report without 6 models

---
+++ latex/create_GAPSLIP_report.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+file_name="GAPSLIP_report"
+
+cat << END_CAT > temp/figure_parameters.sh
+six_models=""
+END_CAT
+
+bash figure_tex.sh
+
+
+rm *.aux *.bbl *.blg *.dvi *.log
+
+# xelatex defaults to PDF version 1.5, so you have to manually set it to 1.7 to be compatible with psconvert PDF files
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+bibtex ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+xelatex --output-driver="xdvipdfmx -v -V 7" ${file_name}
+
+
+texcount -inc  ${file_name}.tex
+
+# clean temporary files
+
+rm *.aux *.bbl *.blg *.dvi *.log

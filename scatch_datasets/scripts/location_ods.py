40338eb600f634ea145bd95f4e5d238ed670c670
Evan Gowan <evangowan@gmail.com>
2024-01-11 12:27:26 +0900

    made it so the script puts the resevoir corrrection value in mixed curves

--- scatch_datasets/scripts/location_ods.py
+++ scatch_datasets/scripts/location_ods.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from pandas_ods_reader import read_ods
 
+
 merged_file = sys.argv[1] # merged ods file
 region_file = sys.argv[2] # csv file with Region field filled out
 reservoir_file = sys.argv[3] # csv file with reservoir corrections field filled out
@@ -36,11 +37,16 @@ sl_data3 = sl_data3.sort_values(by='LAB_ID',ignore_index=True)
 sl_data['Region'] = sl_data2['location']
 
 
+# add the reservoir correction for the marine curve
 
 sl_data3.loc[sl_data3['Curve'] == "marine", 'Reservoir_'] = sl_data3['delta_r']
 sl_data3.loc[sl_data3['Curve'] == "marine", 'Reservoi_1'] = sl_data3['uncertaint']
 
 
+# also do the same for mixed curves
+
+sl_data3.loc[sl_data3['Curve'].str.contains("mixed", na=False), 'Reservoir_'] = sl_data3['delta_r']
+sl_data3.loc[sl_data3['Curve'].str.contains("mixed", na=False), 'Reservoi_1'] = sl_data3['uncertaint']
 
 sl_data['Reservoir_age'] = sl_data3['Reservoir_']
 sl_data['Reservoir_error'] = sl_data3['Reservoi_1']

083cd9da864d35b0182232f6687a6077715852fa
Evan Gowan <evangowan@gmail.com>
2025-05-22 21:38:38 +0900

    modified the map to show newly added points

--- GIS/plot_map.sh
+++ GIS/plot_map.sh
@@ -5,28 +5,43 @@
 J_options="-JR10/15c"
 R_options="-Rg"
 
-plot="data_map.ps"
 
-circle_size=0.10
+
+circle_size=0.12
 circle_pen=" -W0.3p,black "
 
+gmt begin data_map pdf,png A,I+s240p
+  gmt coast ${R_options} ${J_options} -Bx60 -By30   -Wfaint,black -Dc -A500 -G210 --FONT_TITLE=16p 
+    sort -u test_sl.txt > points_sort.txt
+	gmt plot points_sort.txt  ${circle_pen} -Sc${circle_size} -Gred 
 
-gmt pscoast ${R_options} ${J_options} -Bx60 -By30  -P -Wfaint,black -Dc -A500 -G210 -K --FONT_TITLE=16p > ${plot}
-
-gmt psxy test_sl.txt -R -J -P ${circle_pen} -Sc${circle_size} -Ggreen -O -K >> ${plot}
+    sort -u test_sl_2.1.txt > points_sort_old.txt
 
 
-gmt psxy << END_CAT -Y-3 -X-3.2  -P -K -O -R0/1/0/1 -JX10/10  ${circle_pen} -Sc${circle_size} -Ggreen >> ${plot}
-0.3 0.31
+    grep -vf points_sort_old.txt  points_sort.txt > diff_file.txt
+	gmt plot diff_file.txt  ${circle_pen} -Sc${circle_size} -Ggreen 
+
+
+
+
+	gmt plot << END_CAT -Y-3 -X-3.2   -R0/1/0/1 -JX10/10  ${circle_pen} -Sc${circle_size} -Ggreen
+0.3 0.34
 END_CAT
 
-gmt pstext << END_CAT   -JX -R -P  -O -F+f8p+jBL >> ${plot}
-0.33 0.3 Data location
+	gmt plot << END_CAT  -JX10/10  ${circle_pen} -Sc${circle_size} -Gred
+0.3 0.30
 END_CAT
 
-gmt psconvert  ${plot}  -Tf -A
-gmt psconvert  ${plot}  -Tg -A -I+s240p
 
-rm ${plot}
+	gmt text << END_CAT     -F+f8p+jBL 
+0.33 0.33 New data (version 3.0)
+0.33 0.29 Previously entered data
+END_CAT
+
+gmt end 
+#gmt psconvert  ${plot}  -Tf -A
+#gmt psconvert  ${plot}  -Tg -A -I+s240p
+
+
 
 mv data_map.png ../data_map.png

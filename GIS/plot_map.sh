541e548292b154a9b6dfd66be00b187cfe1b1cdb
Evan Gowan <evangowan@gmail.com>
2025-12-29 15:04:51 +0900

    updated point count and maps

--- GIS/plot_map.sh
+++ GIS/plot_map.sh
@@ -8,10 +8,18 @@ R_options="-Rg"
 
 
 circle_size=0.12
+circle_size_small=0.09
 circle_pen=" -W0.3p,black "
 
 gmt begin data_map pdf,png A,I+s240p
   gmt coast ${R_options} ${J_options} -Bx60 -By30   -Wfaint,black -Dc -A500 -G210 --FONT_TITLE=16p 
+
+	awk --field-separator='\t' '{print $2, $1}' Locations_to_add.txt > temp_yet.txt
+    sort -u temp_yet.txt > points_sort_yet.txt
+
+
+	gmt plot points_sort_yet.txt  ${circle_pen} -Ss${circle_size_small} -Gyellow
+
     sort -u test_sl.txt > points_sort.txt
 	gmt plot points_sort.txt  ${circle_pen} -Sc${circle_size} -Gred 
 
@@ -33,9 +41,15 @@ END_CAT
 END_CAT
 
 
+	gmt plot << END_CAT  -JX10/10  ${circle_pen} -Ss${circle_size_small} -Gyellow
+0.3 0.26
+END_CAT
+
+
 	gmt text << END_CAT     -F+f8p+jBL 
 0.33 0.33 New data (version 3.0)
 0.33 0.29 Previously entered data
+0.33 0.25 Yet to be added
 END_CAT
 
 gmt end 
